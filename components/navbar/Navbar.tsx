50cf7b45e9083000b0aed09ad18154f333bac1c9
sethdylan <sethdylanortiz@gmail.com>
2023-11-30 02:12:05 -0800

    dynamic styling of navbar active links

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,12 +1,21 @@
-import Link from 'next/link';
+"use client";
 import React from 'react'
 import styles from './navbar.module.css'
+
+// nextjs
+import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 // icons
 import bug_icon from '@/public/icon-bug.png';
 
 const Navbar = () => {
+
+    // next.js hook - that returns the current pathname
+    // this connects to browser api - therfore, need to make this a client component
+    const get_current_path = usePathname();
+    console.log(get_current_path); // outputs: "/" or "/issues" on google console output since its csr
 
     // map out array for dynamic link input
     const links = [
@@ -20,16 +29,16 @@ const Navbar = () => {
                 <Image
                     src = {bug_icon}
                     alt = "logo"
-                    width = {50}
-                    height = {50}
-                />
-                
+                    width = {40}
+                    height = {40}
+                />     
             </Link>
             <ul className = {styles.ul_container}>
                 { links.map(link => 
-                    <li> <Link 
+                    <li> 
+                        <Link 
                             key = {link.href}
-                            className = {styles.link} 
+                            className = {`${link.href == get_current_path ? styles.link_selected : styles.link}`} 
                             href = {link.href}>{link.label}
                         </Link>
                     </li>
